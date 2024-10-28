8aaff989ae3196f53c6b0c8745102d2ea10945a9
Ana <anacrisduartesantos20@gmail.com>
2024-10-28 17:11:51 -0300

    Alterações active

--- main.js
+++ main.js
@@ -2,7 +2,40 @@ $(document).ready(function() {
     $('#mobile_btn').on('click', function(){
         $('#mobile_menu').toggleClass('active');
     });
+
+    const sections = $('section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function () {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+        const activeIndexDisplay = document.getElementById('activeIndex');
+        const nextButton = document.getElementById('nextButton');
+
+        nextButton.addEventListener('click', () => {
+            activeSectionIndex++;
+            activeIndexDisplay.textContent = activeSectionIndex;
+        });
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 96;
+            const sectionBottom = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                activeSectionIndex = i;
+                return false;
+            }
+        });
+
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+    });
 });
+
 
 const scrollRevealOption = {
     distance: "50px",
@@ -24,3 +57,4 @@ ScrollReveal().reveal(".content-left .buttonEntre", {
     ...scrollRevealOption,
     delay: 1500,
 });
+
