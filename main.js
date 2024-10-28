fa078f4e5284ea31a3aeda9417b6564aed3e87ef
AnaDuarteS <anacrisduartesantos20@gmail.com>
2024-10-28 11:51:47 -0300

    Alterações botao login e span dos cards

--- main.js
+++ main.js
@@ -2,5 +2,25 @@ $(document).ready(function() {
     $('#mobile_btn').on('click', function(){
         $('#mobile_menu').toggleClass('active');
     });
-})
+});
 
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".content-right", {
+    ...scrollRevealOption,
+    origin: "right",
+});
+
+ScrollReveal().reveal(".content-left", {
+    ...scrollRevealOption,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".content-left .buttonEntre", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
